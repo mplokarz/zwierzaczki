136c89b059ed3cf5a8128c471c57f2f041c7f967
FIRST_NAME LAST_NAME <Maciej.Drozynski@networkrail.co.uk>
2022-07-04 13:42:27 +0200

    static method

--- OOP.py
+++ OOP.py
@@ -1,30 +1,44 @@
 class Critter(object):
-    # Zwierzak z atrybutem
-    # Demonstruje tworzenie atrybutów obiektu i uzyskiwanie do nich dostępu
+    #Zwierzak z atrybutem
+    #Demonstruje tworzenie atrybutów obiektu i uzyskiwanie do nich dostępu
+    total = 0
+
+    @staticmethod          #wbudowany dekorator do tworzenia metod statycznych klasy
+    def status():          #tworzymy metodę statyczną, związaną z klasą Critter i wykorzystującą atrybut klasy: total. Ponieważ jest to metoda klasy, nie dajemy parametru self,
+        print("\n Ogólna liczba zwierzaków wynosi", Critter.total)        #który niezbędny jest tylko dla metod obiektu
+    
 
     def __init__(self, name):
-        print("Urodził się nowy zwierzak!")     # specjalna metoda (konstruktor) wywoływana automatycznie zaraz po utworzeniu nowego obiektu klasy Critter
+        print("Urodził się nowy zwierzak!")     #specjalna metoda (konstruktor) wywoływana automatycznie zaraz po utworzeniu nowego obiektu klasy Critter
         self.name = name
+        Critter.total += 1
+
 
     def __str__(self):
         rep = "Obiekt klasy Critter\n"
         rep += "name: " + self.name + "\n"
         return rep
 
-    def talk(self):                             # ta metoda ma tylko jeden parametr (niezbędny), ktorego akurat nie używa, dostarcza metodzie sposób odwołąnai się do samego obiektu
-        print("Cześć! Jestem " + self.name + "\n")
+
+    def talk(self):                             #ta metoda ma tylko jeden parametr (niezbędny), ktorego akurat nie używa, dostarcza metodzie sposób odwołąnia się do samego obiektu
+        print(f"Cześć! Jestem {self.name}.")
+
 
 # część główna
-crit1 = Critter("Reksio")        #konkretyzacja obiektu
-crit1.talk()                     #wywołanie metody talk()
+print("Uzyskanie dostępu do atrybutu klasy Critter.total:", end=" ")
+print(Critter.total)
 
-crit2 = Critter("Bobek")
-crit2.talk()
+print("\nTworzenie zwieczaków.")
+crit1 = Critter("zwierzak 1")
+crit2 = Critter("zwierzak 2")
+crit3 = Critter("zwierzak 3")
+crit4 = Critter("zwierzak 4")
+crit5 = Critter("zwierzak 5")
 
-print("Wyświetlenie obiektu crit1:")
-print(crit1)
-print("Bezpośrednie wyświetlenie wartości atrybutu crit1.name:")
-print(crit1.name)
+Critter.status()
+
+print("\nUzyskanie dostępu do atrybutu klasy poprzez obiekt:", end=" ")
+print(crit1.total)
 
 
 #input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
